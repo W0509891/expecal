1765da1e0b0fcdfa92a1b4bd9f3c4f47c4eb17ed
niyi-16 <dasil.adam@gmail.com>
2025-07-07 01:06:59 -0300

    made calendar looking page

--- script.js
+++ script.js
@@ -10,14 +10,34 @@ function $qsa(clss) {
     return document.querySelectorAll(clss)
 }
 
-let days = 30
+let today = new Date()
+
+const year = today.getFullYear()
+const month = today.getFullYear()
+const day = today.getDate()
+
+let days = 31
 let daysInWeek = 7
 
-$("output").innerText = Math.ceil(days/daysInWeek)
+$("output").innerText = day
 
-let table = $qs("table")
+let tablebody = $("calenderbody")
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+let table = document.createElement('table')
+let dow = document.createElement("tr")
+dow.id = 'daysOfWeek'
+
+table.appendChild(dow)
 count = 1 ;
+
+tablebody.appendChild(table)
+daysOfWeek.forEach(item => {
+    let dayCell = document.createElement("th")
+    dayCell.innerText = item
+    dow.appendChild(dayCell)
+    
+})
 for (let i = 1; i <= Math.ceil(days/daysInWeek) ; i++) {
     let tr = document.createElement("tr")
     for (let j = 1; j <= daysInWeek ; j++) {
@@ -32,12 +52,18 @@ for (let i = 1; i <= Math.ceil(days/daysInWeek) ; i++) {
     table.appendChild(tr)
 }
 
-$qsa("th, td").forEach(e => {
-    e.addEventListener("mouseover", () => {
-        e.style.backgroundColor = '#ff4500'
-    });
-    
-    e.addEventListener("mouseout", () => {
-        e.style
-    })
+// $qsa("th, td").forEach(e => {
+//     e.addEventListener("mouseover", () => {
+//         e.style.backgroundColor = '#00ff49'
+//     });
+//
+//     e.addEventListener("mouseout", () => {
+//         e.style.backgroundColor = 'none';
+//     })
+// })
+
+$qsa('td').forEach(e => {
+    if ( Number(e.innerText) === day){
+        e.style.backgroundColor = "rgba(47,0,255,0.3)"
+    }
 })
