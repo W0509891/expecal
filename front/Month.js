c3f25a925eb3f4cd24bc1bc93c326fc6ad5fafcb
niyi-16 <dasil.adam@gmail.com>
2025-07-16 20:23:27 -0300

    made calendar looking page

---
+++ front/Month.js
@@ -0,0 +1,41 @@
+let month_data =[
+    {
+        day: 1,
+        transaction:[
+            {timestamp: '00:00', type: 'dr', amount: '100', category: 'food'},
+            {timestamp: '01:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '02:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '03:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '04:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '05:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '06:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '07:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '09:00', type: 'cr', amount: '1000', category: 'etransfer'},
+            {timestamp: '10:00', type: 'cr', amount: '200', category: 'etransfer'},
+            {timestamp: '11:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '12:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '13:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '14:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '15:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '16:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '17:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '18:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '19:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '20:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '21:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '22:00', type: 'dr', amount: '200', category: 'misc'},
+            {timestamp: '23:00', type: 'dr', amount: '200', category: 'misc'},
+        ]
+    },
+
+    {
+        day: 10,
+        transaction: [
+            {timestamp: '12:00', type: 'dr', amount: '100', category: 'food'},
+            {timestamp: '13:00', type: 'dr', amount: '100', category: 'food'},
+            {timestamp: '14:00', type: 'dr', amount: '100', category: 'food'},
+            {timestamp: '19:00', type: 'dr', amount: '100', category: 'food'},
+        ]
+    }
+]
+
