56b99ffbfda201a7e72f4c52a42562b36664bfc9
niyi-16 <dasil.adam@gmail.com>
2025-12-25 19:04:22 -0400

    Updated import paths for `AppComponent` and `CalendarComponent` to reflect new folder structure

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {Route, Routes} from '@angular/router';
-import { CalendarComponent} from "./routes/calendar/calendar.component"
+import { CalendarComponent} from "./ui/calendar/calendar.component"
 
 const calendar:Route = {path: "", component: CalendarComponent, title: "Calendar" }
 export const routes: Routes = [calendar];
