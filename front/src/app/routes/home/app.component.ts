9626d00c368b2741f16f825dd39c608c36c16acc
niyi-16 <dasil.adam@gmail.com>
2025-12-25 19:02:45 -0400

    Refactored `AppComponent` to use inline template and updated navbar import path

--- front/src/app/routes/home/app.component.ts
+++ front/src/app/routes/home/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './ui/navbar/navbar.component';
+import {NavbarComponent} from '../../ui/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet />
+  `
 })
 export class AppComponent {
   title = 'expecal';
