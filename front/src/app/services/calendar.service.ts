3384d1659d3ba66e8a978f8aba54a65fbe9dd754
niyi-16 <dasil.adam@gmail.com>
2025-12-28 21:48:01 -0400

    Added `CalendarService` to manage calendar logic, including month view generation, navigation, and day tracking

---
+++ front/src/app/services/calendar.service.ts
@@ -0,0 +1,143 @@
+import {Injectable} from '@angular/core';
+import {CalendarModel, Day, Month} from "../models/CalendarModel";
+
+@Injectable({providedIn: 'root'})
+
+export class CalendarService {
+
+  public calModel: CalendarModel = new CalendarModel()
+  protected _year
+  protected _month: Month
+  protected _current_day: Day
+  protected _weeks: Day[][] = []
+  protected _weekdays: string[]
+
+  protected _cal_view_days: number = 0
+
+  constructor() {
+    this._year = this.calModel.getYear();
+    this._month = this.calModel.getYear().getMonth();
+    this._current_day = this.calModel.getCurrentDay();
+    this._weekdays = Month.DayNames;
+    this.generateMonthView();
+  }
+
+  /**
+   * Generates the array size based on the month start day.
+   */
+  generateArraySize() {
+    //Checks if it starts on a fri/sat & if the month is not feb and is more than 30 da
+    if ((this._month.getStartDay() === 5 || this._month.getStartDay() === 6) && (this._month.getId() !== 1)) {
+      this._cal_view_days = 42
+      return
+    }
+    this._cal_view_days = 35
+  }
+
+  /**
+   * Generates the month view based on the current month object.
+   */
+  generateMonthView() {
+    this.generateArraySize()
+
+    let monthView = []
+    const days = this._month.getDays();
+    const startDay = this._month.getStartDay();
+
+    //last month from the previous year or previous month in the same year
+    const previousMonth = this._month.getId() === 0 ?
+      new Month(11, this._year.getId() - 1, CalendarModel.isLeapYear(this._year.getId() - 1)) :
+      this._year.getMonth(this._month.getId() - 1);
+
+    //first month in next year or next month in the same year
+    const nextMonth = this._month.getId() === 11 ?
+      new Month(0, this._year.getId() + 1, CalendarModel.isLeapYear(this._year.getId() + 1)) :
+      this._year.getMonth(this._month.getId() + 1);
+
+
+    console.log("First Day", this._month.getDay())
+    console.log("weekday", startDay, this._weekdays[startDay])
+
+
+
+    //Fills the array with days from the previous month
+    for (let j = 0; j < startDay; j++) {
+      // noOfDays in prev month less curr month start index + n to get days of prev month
+      monthView[j] = previousMonth.getDay(previousMonth.getDays().length - startDay + (j + 1))
+    }
+
+    //Fills the array with days in the current month
+    days.forEach((day, index) => {
+      monthView[startDay + index] = day
+    })
+
+
+    //Fills the array with days after month
+    for (let i = 1, daysLeft = this._cal_view_days - monthView.length; i <= daysLeft; i++) {
+      monthView.push(nextMonth.getDay(i));
+    }
+
+
+    //Initialize weeks field
+    this._weeks = Array.from(
+      {length: monthView.length / 7},
+      (_, i) => monthView.slice(i * 7, i * 7 + 7)
+    );
+
+    console.log("monthView", monthView)
+    console.log("weeks", this._weeks)
+
+  }
+
+  /**
+   * Changes the current month by the given number of months.
+   * @param num
+   */
+  changeMonthBy(num: number) {
+    const currentDate = CalendarModel.CalDate  // Save the current date
+    // Change the current date by the given number of months
+    CalendarModel.CalDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + num, 1)
+
+    // If the year has changed, update the year object in the calendarService
+    if (CalendarModel.CalDate.getFullYear() !== currentDate.getFullYear()) {
+      this.calModel.setYear(CalendarModel.CalDate.getFullYear())
+    }
+
+    // Update the current month object in the calendarService
+    this._month = this.calModel.getYear().getAllMonths()[CalendarModel.CalDate.getMonth()]
+    this.generateMonthView()  // Regenerate the month view based on the new month object
+
+  }
+
+
+  // Returns the current year object
+  getYear() {
+    return this._year;
+  }
+
+  // Returns the current month object
+  getMonth(): Month {
+    return this._month;
+  }
+
+  // Returns the current day object of the month
+  getCurrent_day(): Day {
+    return this._current_day;
+  }
+
+  // Returns a 2d [5|6][7] array representing the week view of the month
+  getWeeks(): Day[][] {
+    return this._weeks;
+  }
+
+  // Returns the weekdays of the month
+  getWeekdays(): string[] {
+    return this._weekdays;
+  }
+
+  // Returns The number of days being displayed in the calendar view
+  getCal_view_days(): number {
+    return this._cal_view_days;
+  }
+
+}
