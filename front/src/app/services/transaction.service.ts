a7d62750376e90c1ed4e586b6034e3c87d76f531
niyi-16 <dasil.adam@gmail.com>
2026-02-02 00:42:47 -0400

    Modified transaction service to make my life easier

--- front/src/app/services/transaction.service.ts
+++ front/src/app/services/transaction.service.ts
@@ -1,50 +1,25 @@
 import {Transaction} from '../models/Transaction.interface';
-import {Injectable} from '@angular/core';
+import {Injectable, signal} from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class TransactionService {
-  static Counter: number = 0
-  api_key: string = "http://localhost:8000/transactions"
+  private readonly api_key: string = "http://localhost:8000/transactions"
 
-  Transactions: Transaction[] = []
-  // fetched: boolean = false
+  transaction_store = signal<Transaction[]>([])
+  transactions = signal<Record<string, Transaction[]>>({})
 
-  constructor() {}
-
-
-  async fetchTransactions(date: string):Promise<Transaction[]>{
-
-    console.log(++TransactionService.Counter, "Api call made")
-    const response = await fetch(`${this.api_key}?date=${date}`)
-
-    return await response.json()
-
-
+  constructor() {
   }
 
+  async fetchTransactionByDate(date: string): Promise<Transaction> {
+    const response = await fetch(`${this.api_key}?date_on=${date}`)
 
+    return await response.json()
+  }
 
-  filterByDate(filter: string): Transaction[] {
-
-    // const transactions = async() => await this.fetchTransactions()
-
-    let dateSet: Set<any> = new Set()
-
-    // console.log(transactions)
-    this.Transactions.forEach(transaction => {
-      dateSet.add(transaction.date)
-    })
-
-    let dates: string[] = Array.from(dateSet)
-
-    let filteredDates: Record<string, Transaction[]> = {}
-
-    for (let date of dates) {
-      filteredDates[date] = this.Transactions.filter(transaction => transaction.date === date)
-    }
-
-    // console.log("Filtered dates" ,filteredDates[dates[1]])
-    return filteredDates[filter]
+  async fetchTransactionByRange(from: string, to: string): Promise<Record<string, Transaction[]>> {
+    const response = await fetch(`${this.api_key}?date_from=${from}&date_to=${to}`)
+    return await response.json()
   }
 }
 
