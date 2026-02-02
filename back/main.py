a748d391eae97c16c94a94bf807252647a3a1f5b
niyi-16 <dasil.adam@gmail.com>
2026-02-02 00:32:25 -0400

    Modified get transaction query to aggregate data better

--- back/main.py
+++ back/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import dotenv
 
@@ -40,19 +41,53 @@ async def say_hello(name: str):
 
 
 @app.get("/transactions")
-async def get_transactions(date: str = None):
+async def get_transactions(date_from: str = None, date_to: str = None, date_on: str = None):
     db = get_db()
     db_cursor = db.cursor(dictionary=True)
 
     try:
-        if date:
-            query = ("SELECT * FROM transactions WHERE transactions.date = %s")
-            db_cursor.execute(query, date)
+        if date_on:
+            query = f"SELECT * FROM transactions WHERE transactions.date = '{date_on}'"
+            db_cursor.execute(query)
+
+        elif date_from and not date_to:
+            return {"message": "only from"}
+
+        elif date_to and not date_from:
+            return {"message": "only to"}
+
+        elif date_from and date_to:
+            query = f"""
+            SELECT JSON_OBJECTAGG( d.`date`, d.txns ) AS transactions
+            FROM (
+                    SELECT t.`date`, JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                                'id', t.id,
+                                'type', t.type,
+                                'description', t.description,
+                                'amount', t.amount,
+                                'balance', t.balance,
+                                'currency', t.currency,
+                                'account_name', a.type,
+                                'bank_name', fi.name
+                )
+            ) AS txns
+            FROM saveify.transactions t
+            join saveify.accounts a on t.account_id = a.id
+            join saveify.financial_institution fi on a.bank_id = fi.id
+            WHERE t.`date` BETWEEN '{date_from}' AND '{date_to}'
+            GROUP BY t.`date`
+            ) AS d;"""
+
+            db_cursor.execute(query)
+            result = db_cursor.fetchall()
+
+            result = json.loads(result[0]["transactions"])
+            return result
 
         else:
             query = "SELECT * FROM transactions"
             db_cursor.execute(query)
-
 
         result = db_cursor.fetchall()
 
