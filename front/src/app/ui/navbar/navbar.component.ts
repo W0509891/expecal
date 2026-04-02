298a246be853a6d207f6374026d3febd2f934fcb
niyi-16 <dasil.adam@gmail.com>
2026-04-02 14:04:51 -0300

    Enhance navbar style,functionality, including improved layout, button interactions, and date picker integration

--- front/src/app/ui/navbar/navbar.component.ts
+++ front/src/app/ui/navbar/navbar.component.ts
@@ -1,32 +1,52 @@
 import {Component, inject} from '@angular/core';
 import {CalendarService} from "../../services/calendar.service"
-import {SidebarService} from "../../ui/sidebar/sidebar.component"
+import {SidebarService} from "../sidebar/sidebar.component"
+import {UploadService} from "../upload/upload.component"
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   styleUrl: './navbar.component.scss',
   template: `
     <div class="navbar-container">
       <div class="navbar">
 
         <div class="left">
-          <div (click)="onClicked()">
-            <img src="/assets/svg/hamburger.svg">
-          </div>
+          <svg (click)="SidebarService.showNav()" class="svg">
+            <use href="/assets/svg/hamburger.svg#icon"></use>
+          </svg>
         </div>
 
         <div class="center">
-          <button id="back" (click)="onMonthChange(-1)"><-</button>
           <h3>Expense Cal</h3>
-          <button id="front" (click)="onMonthChange(1)">-></button>
+        </div>
+
+        <div>
+            <button (click)="UploadService.showUploadForm()" id="upload">Upload</button>
         </div>
 
         <div class="right">
+          <button id="back" (click)="onMonthChange(-1)">
+            <svg class="svg">
+              <use href="/assets/svg/arrow.svg" transform="rotate(90 11 13)"></use>
+            </svg>
+          </button>
           <div>
-            <p>{{CalendarService.calModel.CalDate.toDateString()}}</p>
+            <input type="date"
+                   class="date-picker"
+                   (focus)="dateClick($event.target)"
+                   (change)="changeDate(formDate.value)"
+                   [formControl]="formDate"
+            >
+<!--            <p>{{CalendarService.calModel.CalDate.toDateString()}}</p>-->
           </div>
+          <button id="front" (click)="onMonthChange(1)">
+            <svg class="svg">
+              <use href="/assets/svg/arrow.svg" transform="rotate(270 13 13)"></use>
+            </svg>
+          </button>
         </div>
 
       </div>
@@ -38,13 +58,30 @@ import {SidebarService} from "../../ui/sidebar/sidebar.component"
 export class NavbarComponent {
   CalendarService: CalendarService = inject(CalendarService);
   SidebarService: SidebarService = inject(SidebarService)
+  UploadService: UploadService= inject(UploadService)
+
+  formDate = new FormControl(this.CalendarService.getCurrent_day()!!.toString('short') as string)
 
   constructor() {}
 
-  onClicked() {
-    this.SidebarService.toggle()
+  changeDate(formDate: string|null) {
+    const [year, month, day] = formDate?.split('-') as [string, string, string]
+    this.CalendarService.setNewDate(Number(year),Number(month), Number(day))
   }
   onMonthChange(step: number) {
     this.CalendarService.changeMonthBy(step);
+    this.formDate.setValue(this.CalendarService.getCurrent_day()!!.toString('short') as string);
+  }
+
+  dateClick(event: any) {
+    console.log(event)
+    if (event.showPicker) {
+      try {
+        event.showPicker()
+      }
+      catch (e:any) {
+        console.error("Error: " + e.message);
+      }
+    }
   }
 }
